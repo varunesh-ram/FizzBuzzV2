667a650f6eba596fb036c176d34b864148720091
Varunesh Ram ThangaPerumal <varuneshram@yahoo.com>
2020-01-01 19:02:38 +0530

    Refactored FizzBuzz.java to print Fizz for all input divisible by 3

--- src/main/java/com/practise/exercise/FizzBuzz.java
+++ src/main/java/com/practise/exercise/FizzBuzz.java
@@ -4,9 +4,7 @@ public class FizzBuzz {
 
 	public String generate(int inputValue) {
 		String outputValue = String.valueOf(inputValue);
-		if(inputValue ==3)
-			outputValue=  "Fizz";
-		if(inputValue ==6)
+		if(inputValue%3 == 0)
 			outputValue=  "Fizz";
 		return outputValue;
 	}
